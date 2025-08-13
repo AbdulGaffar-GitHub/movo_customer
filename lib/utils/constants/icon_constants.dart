823b25f355e11cfcc863a3b5b788a9d100531ca0
abdulgaffar91 <abdulgaffar@innoitlabs.com>
2025-08-13 12:55:43 +0530

    first commit

---
+++ lib/utils/constants/icon_constants.dart
@@ -0,0 +1,54 @@
+class AppIcons {
+  static const String iconsPath = 'assets/icons';
+
+  //Icons
+
+  //onboarding
+  static const String appLogo = '$iconsPath/app_logo_lg.svg';
+
+  // //common
+  static const String add = '$iconsPath/add.svg';
+  static const String arrowDown = '$iconsPath/arrow_down.svg';
+  static const String arrowUp = '$iconsPath/arrow_up.svg';
+  static const String arrowBack = '$iconsPath/arrow_back.svg';
+  static const String arrowForward = '$iconsPath/arrow_forward.svg';
+  static const String search = '$iconsPath/search.svg';
+  static const String notificationOutlined= '$iconsPath/notification_outlined.svg';
+  static const String location= '$iconsPath/location.svg';
+  static const String location1= '$iconsPath/location1.svg';
+  static const String phone= '$iconsPath/phone.svg';
+  static const String skip= '$iconsPath/skip_icon.svg';
+  static const String calender= '$iconsPath/calender.svg';
+  static const String clear= '$iconsPath/clear.svg';
+  static const String remove= '$iconsPath/remove.svg';
+  static const String download= '$iconsPath/download.svg';
+
+
+  //bottombar
+  static const String homeOutlined= '$iconsPath/home_outlined.svg';
+  static const String ordersOutlined= '$iconsPath/orders_outlined.svg';
+  static const String reportsOutlined= '$iconsPath/report_outlined.svg';
+  static const String profileOutlined= '$iconsPath/profile_outlined.svg';
+
+  static const String home= '$iconsPath/home.svg';
+  static const String orders= '$iconsPath/orders.svg';
+  static const String reports= '$iconsPath/report.svg';
+  static const String profile= '$iconsPath/profile.svg';
+  static const String notification= '$iconsPath/notification.svg';
+
+//Home Screen
+  // static const String location = '$iconsPath/location.svg';
+
+  //profile screen
+  // static const String graduationCap = '$iconsPath/graduation_cap.svg';
+  static const String order1= '$iconsPath/order1.svg';
+  static const String editprofile= '$iconsPath/edit_profile.svg';
+  static const String language= '$iconsPath/language.svg';
+  static const String termsandcond= '$iconsPath/terms&conditions.svg';
+  static const String privacy= '$iconsPath/privacy.svg';
+  static const String delete= '$iconsPath/delete.svg';
+  static const String logout= '$iconsPath/logout.svg';
+  static const String cashFree= '$iconsPath/cashFree.svg';
+
+
+}
